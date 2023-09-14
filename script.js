c39a76728f381a20e15812bd8e6872399f8a596a
yadunandanks <108681399+yadunandanks@users.noreply.github.com>
2023-09-14 13:31:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let index = 0;
+
+function changeColors() {
+  let colors = ["red", "blue", "yellow", "green", "purple", ""];
+
+  document.getElementsByTagName("body")[0].style.background = colors[index++];
+
+  if (index > colors.length - 1) index = 0;
+}
